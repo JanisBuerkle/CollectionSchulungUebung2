2009ff88fe64a2fd88079995a5d471ca789d6610
Janis Bürkle <bks@teamtechnik.com>
2024-01-04 14:32:14 +0100

    v

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿List<string> list = new List<string>();
+list.Add("Hallo");
+list.Add("Welt");
+list.Add("!");
+
+Console.WriteLine(list[1]);
+list.RemoveAt(1);
+list.Clear();
+
+List<object> list2 = new List<object>();
+list2.Add("Hallo Welt!");
+list2.Add(1);
+list2.Add(new Exception());
